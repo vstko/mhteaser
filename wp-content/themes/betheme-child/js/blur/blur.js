92b61ebb7289d2e131d9c026157282f1205bc386
reguloleonardi <regulo.leonardi@gmail.com>
2018-11-11 17:44:01 +0100

    Blur images widget update

--- wp-content/themes/betheme-child/js/blur/blur.js
+++ wp-content/themes/betheme-child/js/blur/blur.js
@@ -7,16 +7,15 @@
             window.addEventListener('scroll', function() {
                 var screenHeight = window.innerHeight;
                 imgs.forEach(function(el) {
-                    var pxMid = el.getBoundingClientRect().top - (el.clientHeight/2);
-                    console.log(pxMid);
-                    var percentage = (( screenHeight - pxMid ) / screenHeight) * 100 ;
+                    var pxMid = (el.getBoundingClientRect().top - (el.clientHeight/2));
+                    var percentage = ((( screenHeight - pxMid ) / screenHeight) * 100)/2 ;
                     var opacityVal = pxMid / 200;
-                    if(percentage < 80){
+                    if(percentage < 40) {
                         el.style.opacity =  1;
                     }
-                    else if(percentage > 80 && percentage < 97){
+                    else if(percentage > 40 && percentage < 50) {
                         el.style.opacity =  opacityVal;
-                    }else if(percentage > 97){
+                    }else if(percentage > 50) {
                         el.style.opacity =  -opacityVal;
                     }
                 });
