1896939ba9217cd13f18c585d424eeef7651d4c9
reguloleonardi <regulo.leonardi@gmail.com>
2018-11-13 21:32:42 +0100

    Fonts & Js blur breakpoint

--- wp-content/themes/betheme-child/js/blur/blur.js
+++ wp-content/themes/betheme-child/js/blur/blur.js
@@ -7,21 +7,24 @@
         blurImg: function (imgs) {
             window.addEventListener('scroll', function() {
                 var screenHeight = window.innerHeight;
-                imgs.forEach(function(el) {
-                    var percentage;
-                    var opacityVal;
-                    var pxMid = el.getBoundingClientRect().top + (el.clientHeight/2) - (screenHeight/2);
-                    percentage = ((( screenHeight - pxMid ) / screenHeight) * 100)/2 ;
-                    opacityVal = pxMid / 200;
-                    if(percentage < 38) {
-                        el.style.opacity =  1;
-                    }
-                    else if(percentage > 38 && percentage < 50) {
-                        el.style.opacity =  opacityVal;
-                    }else if(percentage > 50) {
-                        el.style.opacity =  -opacityVal;
-                    }
-                });
+                var screenWidth = window.innerWidth;
+                if(screenWidth < 769){
+                    imgs.forEach(function(el) {
+                        var percentage;
+                        var opacityVal;
+                        var pxMid = el.getBoundingClientRect().top + (el.clientHeight/2) - (screenHeight/2);
+                        percentage = ((( screenHeight - pxMid ) / screenHeight) * 100)/2 ;
+                        opacityVal = pxMid / 200;
+                        if(percentage < 38) {
+                            el.style.opacity =  1;
+                        }
+                        else if(percentage > 38 && percentage < 50) {
+                            el.style.opacity =  opacityVal;
+                        }else if(percentage > 50) {
+                            el.style.opacity =  -opacityVal;
+                        }
+                    });
+                }
             });
         },
         init: function () {
