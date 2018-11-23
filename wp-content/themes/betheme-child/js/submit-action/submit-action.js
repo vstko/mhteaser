6f731e7f6fd3f17fb09581403d8b37b9a269ed95
reguloleonardi <regulo.leonardi@gmail.com>
2018-11-23 22:10:21 +0100

    Revert submit action js

--- wp-content/themes/betheme-child/js/submit-action/submit-action.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    "use strict";
-    $('#mc-embedded-subscribe-form').on('submit', function(e) {
-            e.preventDefault();
-            var data = $("#mc-embedded-subscribe-form :input").serializeArray();
-            console.log(data);
-        });
-
-})(jQuery);
